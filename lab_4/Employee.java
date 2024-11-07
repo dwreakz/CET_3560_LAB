f6fcb44ac5c66e8ec2c40d9233863f81b6ef0234
dwreakz <dereckchetram123@gmail.com>
2024-11-07 11:10:06 -0500

    Lab1 DONE, Lab2 DONE,Lab3 DONE,Lab4 3/5

---
+++ lab_4/Employee.java
@@ -0,0 +1,67 @@
+package lab_4;
+
+public class Employee {
+    public int getHours() {
+        return baseHours;                // 40 hours/week
+    }
+    public double getSalary() {
+        return baseSalary;               // $40,000.00
+    }
+    public int getVacationDays() {
+        return baseVacationDays;         // 10 days
+    }
+
+    public String getVacationForm() {
+        return baseVacationForm;         // yellow
+    }
+    // These are so that test cases can change the base values
+    // and make sure that subclasses also change their values.
+    // (Your code that you submit is NOT supposed to directly use, call,
+    // or modify the values below!  They are here only to help our site
+    // successfully test your code.  Please ignore them.)
+
+    private int baseHours = 40;
+    private double baseSalary = 40000.0;
+    private int baseVacationDays = 10;
+    private String baseVacationForm = "yellow";
+
+    public final void setBaseHours(int hours) {
+        baseHours = hours;
+    }
+    public final void setBaseSalary(double salary) {
+        baseSalary = salary;
+    }
+    public final void setBaseVacationDays(int days) {
+        baseVacationDays = days;
+    }
+    public final void setBaseVacationForm(String form) {
+        baseVacationForm = form;
+    }
+    public static class Lawyer extends Employee {
+        // overrides getVacationDays from Employee class
+        public int getVacationDays() {
+            return super.getVacationDays() + 5;
+        }
+
+        // overrides getVacationDays from Employee class
+        public String getVacationForm() {
+            return lawyerVacationForm;
+        }
+
+        // this is the Lawyer's added behavior
+        public String sue() {
+            return "I'll see you in court!";
+        }
+        // these are so that test cases can change the Lawyer values
+        // and make sure that subclasses also change
+        private String lawyerVacationForm = "pink";
+
+        public final void setVacationForm(String form) {
+            lawyerVacationForm = form;
+        }
+    }
+    public static class StanFordLawyer extends Employee {
+        public int getVacationDays() {
+        }
+    }
+}
--- lab_4/StanfordLawyer.java
+++ lab_4/StanfordLawyer.java
@@ -1,2 +1,23 @@
 package lab_4;
 
+public class StanfordLawyer extends Employee {
+    @Override
+    public double getSalary() {
+        return super.getSalary() * 1.2;
+    }
+    @Override
+    public int getVacationDays() {
+        return super.getVacationDays() + 3;
+    }
+    public String getVacationForm() {
+        return "pinkpinkpinkpink";
+    }
+
+    public static void main(String[] args) {
+        StanfordLawyer stanfordlawyer = new StanfordLawyer();
+
+        System.out.println(stanfordlawyer.getSalary());
+        System.out.println(stanfordlawyer.getVacationDays());
+        System.out.println(stanfordlawyer.getVacationForm());
+    }
+}
